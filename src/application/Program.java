68e04c0a35f7adefbf6470a9d27fc6aebaf0f703
Launyx <lunyx.castelhano@gmail.com>
2024-05-03 15:57:09 -0300

    Operações de conjunto

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,25 +1,24 @@
 package application;
 
-import java.util.HashSet;
-import java.util.LinkedHashSet;
+import java.util.Arrays;
 import java.util.Set;
 import java.util.TreeSet;
 
 public class Program {
     public static void main(String[] args) {
-        
-        //Set<String> set = new HashSet<>();  // HashSet é o mais rápido, mas não garante a ordem
-        //Set<String> set = new TreeSet<>();
-        Set<String> set = new LinkedHashSet<>();
-
-        set.add("TV");
-        set.add("Tablet");
-        set.add("Notebook");
-        
-        set.removeIf(x -> x.charAt(0) == 'T');
-
-        for (String p : set){
-            System.out.println(p);
-        }
-    }   
+        Set<Integer> a = new TreeSet<>(Arrays.asList(0,2,4,5,6,8,10));
+        Set<Integer> b = new TreeSet<>(Arrays.asList(5,6,7,8,9,10));
+        //union
+        Set<Integer> c = new TreeSet<>(a);
+        c.addAll(b);
+        System.out.println(c);
+        //intersection
+        Set<Integer> d = new TreeSet<>(a);
+        d.retainAll(b); // Somente elementos que tem nos dois conjuntos
+        System.out.println(d);
+        //difference
+        Set<Integer> e = new TreeSet<>(a);
+        e.removeAll(b); // Remove do conjunto copiado de a, todo que também estão no conjunto b
+        System.out.println(e);
+    }
 }
